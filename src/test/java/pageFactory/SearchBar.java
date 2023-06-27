bbd17a935934dbcc3ba742f735ef7ece3dd0fdcd
GhasemiMilad <miladghasemi.mg@gmail.com>
2023-06-27 18:26:16 -0400

    MiladsTestCases

--- src/test/java/pageFactory/SearchBar.java
+++ src/test/java/pageFactory/SearchBar.java
@@ -1,26 +1,39 @@
 package pageFactory;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import Utilities.Driver;
 
 public class SearchBar {
 
-	
 	public SearchBar() {
 		PageFactory.initElements(Driver.getDriver(), this);
 
 	}
+
+	/*
+	 * Declare the "FindBy elements here" for your page: For example:
+	 * 
+	 * @FindBy(LocatorForElementLikeXpath="element") public WebElement
+	 * nameYourFindByAnnotationElementHere;
+	 * 
+	 * Like this one below:
+	 * 
+	 * @FindBy(xpath="//h6[text()='Dashboard']") public WebElement dashboardText;
+	 * 
+	 */
+
+	@FindBy(id = "boost-pfs-search-box-0")
+	public WebElement searchTextBox;
+	@FindBy(xpath = "//*[@id=\"header\"]/div[2]/div/div[3]/div/div[2]/form/button")
+	public WebElement searchButton;
 	
-/*Declare the "FindBy elements here" for your page: For example:
+	@FindBy(xpath="//*[@id=\"boost-pfs-filter-tree\"]/div/div[3]/div[3]/div[2]/div/div/div[2]/div[1]/div[3]/div")
+	public WebElement increaseSliderAttribute;
 	
-	@FindBy(LocatorForElementLikeXpath="element")
-	public WebElement nameYourFindByAnnotationElementHere;
-	
-	Like this one below:
-	
-	@FindBy(xpath="//h6[text()='Dashboard']")
-	public WebElement dashboardText;
-	
-	*/
+	@FindBy(xpath="//*[@id=\"boost-pfs-filter-tree\"]/div/div[3]/div[3]/div[2]/div/div/div[2]/div[1]/div[3]/div/div")
+	public WebElement moveSlider;
+
 }
---
+++ target/test-classes/features/Milad.feature
@@ -0,0 +1,30 @@
+
+Feature: Search Functionality
+
+Background:
+    Given the user is on the Home page
+	  When the user write the desired item on search box
+		
+
+#
+  		Scenario: search for an item
+      And click on the desired item
+      Then the user should be able to see the item
+      
+      
+      Scenario: select specific category
+      When the user gets the search result
+      And mark the desired category
+      And click on the desired item
+      Then the user should be able to see the result of seleced category
+      
+      
+      Scenario: select specific Brand
+      When the user gets the search result
+      And mark the desired brand
+      And click on the desired item
+      Then the user should be able to see the result of seleced brand
+      
+      Scenario: slider test for search
+      When the user adjust the slider to specific price
+      Then the user should be able to see the result for selected price
